8a0d3f170bd4f2c5e8b06869c8293f53e8dcc023
Jaye-python <olalekanjayeola@gmail.com>
2022-09-12 20:54:40 +0100

    Added ENV variables

--- user_api_project/settings.py
+++ user_api_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9puy79^vf-ii#k$f3981!urr&&j#3(9zax+u=zbl3t+-r=z3pw'
+SECRET_KEY = ENV.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
