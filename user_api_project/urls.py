f3dba987e3404c5eb483f2a6994cff8b8ae958cb
Jaye-python <olalekanjayeola@gmail.com>
2022-09-15 15:12:41 +0100

    Seed the DB

--- user_api_project/urls.py
+++ user_api_project/urls.py
@@ -6,14 +6,16 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('seeddb/', views.seedDB, name='seeddb'),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    
+        
 ]
